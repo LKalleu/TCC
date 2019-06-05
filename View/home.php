81a654683cadfc7cf7dfef32db7b18a5684a310c
LKalleu <kalleu157@gmail.com>
2019-06-05 09:48:05 -0300

    Inserindo a tela HOME

---
+++ View/home.php
@@ -0,0 +1,27 @@
+<?php
+//Conexão
+include_once '../Model/db_connect.php';
+//Header
+include_once '../Includes/header.php';
+//Mensagem
+include_once '../Includes/mensagem.php';
+?>
+<br class="hide-on-small-only">
+<br class="hide-on-small-only">
+<br class="hide-on-small-only">
+<br class="hide-on-small-only">
+<br class="hide-on-small-only">
+
+<div class="row" style="padding: 10px">
+  <div class="col s12 m6 center-align">
+    <h4 class="light" style="margin-top: 30%; border: 5px solid #2196f3; border-radius: 10px;">Mercearia São José</h4>
+  </div>
+  <div class="col s12 m6 white" style="border: 10px solid #2196f3; border-radius: 10px;">
+    <img src="../Public/img/homeimage.png" alt="" width="100%">
+  </div>
+</div>
+
+<?php
+//Footer
+  include_once '../Includes/footer.php';
+?>
