cba8636d25fccd7d9205a2c2309101c7d9bf7d10
LKalleu <kalleu157@gmail.com>
2019-06-05 09:51:13 -0300

    Ajeitando a tela home

--- View/home.php
+++ View/home.php
@@ -14,7 +14,14 @@ include_once '../Includes/mensagem.php';
 
 <div class="row" style="padding: 10px">
   <div class="col s12 m6 center-align">
-    <h4 class="light" style="margin-top: 30%; border: 5px solid #2196f3; border-radius: 10px;">Mercearia São José</h4>
+    <h4 class="light" style="margin-top: 20%; border: 5px solid #2196f3; border-radius: 10px;">Mercearia São José</h4>
+    <p class="" style="border: 3px solid #2196f3; border-radius: 10px;">Local:
+      <br>
+      Contato:
+      <br>
+      E-Mail:
+      <br>
+    </p>
   </div>
   <div class="col s12 m6 white" style="border: 10px solid #2196f3; border-radius: 10px;">
     <img src="../Public/img/homeimage.png" alt="" width="100%">
