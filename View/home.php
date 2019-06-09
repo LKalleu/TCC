89341dece5980f0158c31bcb3151361c1117479f
LKalleu <kalleu157@gmail.com>
2019-06-09 11:09:14 -0300

    Melhorando a Interface "Home"

--- View/home.php
+++ View/home.php
@@ -6,25 +6,25 @@ include_once '../Includes/header.php';
 //Mensagem
 include_once '../Includes/mensagem.php';
 ?>
-<br class="hide-on-small-only">
-<br class="hide-on-small-only">
-<br class="hide-on-small-only">
-<br class="hide-on-small-only">
-<br class="hide-on-small-only">
 
-<div class="row" style="padding: 10px">
-  <div class="col s12 m6 center-align">
-    <h4 class="light z-depth-2" style="margin-top: 20%; border: 5px solid #2196f3; border-radius: 10px;">Mercearia São José</h4>
-    <p class="z-depth-2" style="border: 3px solid #2196f3; border-radius: 10px;">Local:
-      <br>
-      Contato:
-      <br>
-      E-Mail:
-      <br>
+<div class="row">
+  <div class="col s12 m6 offset-m3 center-align white">
+    <h4 class="light z-depth-2" style="border: 1px solid #2196f3; border-radius: 10px;">Mercearia São José</h4>
+    <p class="z-depth-2" style="border: 1px solid #2196f3; border-radius: 10px;">
+      Local: Av.Rua, Bairro Tal ,222 |
+      Contato: (99) 9 9999-9999 |
+      E-Mail: merceariasaojose@gmail.com
     </p>
   </div>
-  <div class="col s12 m6 white z-depth-2" style="border: 10px solid #2196f3; border-radius: 10px;">
+</div>
+<div class="row" style="padding: 10px">
+  <div class="col s12 m8 offset-m2 white z-depth-2" style="border-radius: 10px;">
     <img src="../Public/img/homeimage.png" alt="" width="100%">
+  </div>
+</div>
+<div class="row">
+  <div class="col s12 m6 offset-m3 center-align">
+
   </div>
 </div>
 
